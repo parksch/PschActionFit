48e0af7289cb8c224020e2449c2599a6b99729bb
parksch <33758744+parksch@users.noreply.github.com>
2025-05-13 21:17:38 +0900

    FEAT Stage Editor

--- Assets/Project/Scripts/StageEditor/StageEditorManager.cs
+++ Assets/Project/Scripts/StageEditor/StageEditorManager.cs
@@ -1,8 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public partial class StageEditorManager : MonoBehaviour
 {
     static public StageEditorManager Instance;
+    [SerializeField] float groundOffset;
+
+    [SerializeField] Transform groundParent;
+
+    [SerializeField] GameObject groundPrefab;
+    [SerializeField] List<GameObject> gridObjects;
 
     void Awake()
     {
@@ -17,6 +24,12 @@ public partial class StageEditorManager : MonoBehaviour
 
     public void CreateGrid()
     {
+        foreach (GameObject go in gridObjects)
+        {
+            Destroy(go);
+        }
+        gridObjects.Clear();
+
         int xMax = 0;
         int.TryParse(xText.text, out xMax);
         int yMax = 0;
@@ -26,14 +39,16 @@ public partial class StageEditorManager : MonoBehaviour
         {
             for (int x = 0; x < xMax; x++)
             {
-
+                GameObject ground = Instantiate(groundPrefab, groundParent);
+                ground.transform.position = new Vector3(-(xMax/2f) + x * groundOffset, 0, (yMax / 2f) - y * groundOffset);
+                gridObjects.Add(ground);
             }
         }
     }
 
     public void OnClickCreateButton()
     {
-
+        CreateGrid();
     }
 }
 
